55b3f918cf81528af782c5db2953bd88386a790d
tomerun <tomerunmail@gmail.com>
2017-08-16 14:55:44 +0900

    use logger

--- um.cr
+++ um.cr
@@ -1,3 +1,5 @@
+require "logger"
+
 class UniversalMachine
   def initialize(program_file : String)
     file = File.new(program_file)
@@ -17,6 +19,12 @@ class UniversalMachine
     @dump_file = File.new("decompressed.um", "w")
     @input_str = "(\\b.bb)(\\v.vv)06FHPVboundvarHRAk"
     @input_pos = 0
+    @logger = Logger.new(STDERR)
+    @logger.level = Logger::WARN
+    @logger.formatter = Logger::Formatter.new do |severity, datetime, progname, message, io|
+      label = severity.unknown? ? "ANY" : severity.to_s
+      io << label[0] << ", " << message
+    end
   end
 
   def start
@@ -33,29 +41,29 @@ class UniversalMachine
     opcode = platter >> 28
     case opcode
     when 0 # Conditional Move
-      STDERR.puts("r[#{reg_a}] <- r[#{reg_b}] if r[#{reg_c}] #{@register}")
+      @logger.info("r[#{reg_a}] <- r[#{reg_b}] if r[#{reg_c}] #{@register}")
       @register[reg_a] = @register[reg_b] if @register[reg_c] != 0
     when 1 # Array Index
       ar = @arrays[@register[reg_b]][0]
       if ar.size <= @register[reg_c]
-        STDERR.puts("#{ar.size}  #{@register[reg_c]}")
+        @logger.error("#{ar.size}  #{@register[reg_c]}")
       end
-      STDERR.puts("r[#{reg_a}] = arr[#{@register[reg_b]}][#{@register[reg_c]}] #{@register}")
+      @logger.info("r[#{reg_a}] = arr[#{@register[reg_b]}][#{@register[reg_c]}] #{@register}")
       @register[reg_a] = ar[@register[reg_c]]
     when 2 # Array Amendment
-      STDERR.puts("arr[#{@register[reg_a]}][#{@register[reg_b]}] = r[#{reg_c}] #{@register}")
+      @logger.info("arr[#{@register[reg_a]}][#{@register[reg_b]}] = r[#{reg_c}] #{@register}")
       @arrays[@register[reg_a]][0][@register[reg_b]] = @register[reg_c]
     when 3 # Addition
-      STDERR.puts("r[#{reg_a}] <- r[#{reg_b}] + r[#{reg_c}] #{@register}")
+      @logger.info("r[#{reg_a}] <- r[#{reg_b}] + r[#{reg_c}] #{@register}")
       @register[reg_a] = @register[reg_b] + @register[reg_c]
     when 4 # Multiplication
-      STDERR.puts("r[#{reg_a}] <- r[#{reg_b}] * r[#{reg_c}] #{@register}")
+      @logger.info("r[#{reg_a}] <- r[#{reg_b}] * r[#{reg_c}] #{@register}")
       @register[reg_a] = @register[reg_b] * @register[reg_c]
     when 5 # Division
-      STDERR.puts("r[#{reg_a}] <- r[#{reg_b}] / r[#{reg_c}] #{@register}")
+      @logger.info("r[#{reg_a}] <- r[#{reg_b}] / r[#{reg_c}] #{@register}")
       @register[reg_a] = @register[reg_b] / @register[reg_c]
     when 6 # Not-And
-      STDERR.puts("r[#{reg_a}] <- ~(r[#{reg_b}] & r[#{reg_c})] #{@register}")
+      @logger.info("r[#{reg_a}] <- ~(r[#{reg_b}] & r[#{reg_c})] #{@register}")
       @register[reg_a] = ~(@register[reg_b] & @register[reg_c])
     when 7 # Halt
       return false
@@ -64,10 +72,10 @@ class UniversalMachine
       if !@arrays.has_key?(@register[reg_b])
         @arrays[@register[reg_b]] = [] of Array(UInt32)
       end
-      STDERR.puts("alloc:#{@register[reg_b]} #{@register[reg_c]}")
+      @logger.info("alloc:#{@register[reg_b]} #{@register[reg_c]}")
       @arrays[@register[reg_b]] << Array(UInt32).new(@register[reg_c], 0_u32)
     when 9 # Abandonment
-      STDERR.puts("abandon:#{@register[reg_c]}")
+      @logger.info("abandon:#{@register[reg_c]}")
       raise @register[reg_c].to_s if !@arrays.has_key?(@register[reg_c])
       @arrays[@register[reg_c]].shift
     when 10 # Output
@@ -75,7 +83,7 @@ class UniversalMachine
         @dump_file.write_byte(@register[reg_c].to_u8)
       else
         print @register[reg_c].unsafe_chr
-        STDERR.puts(@register[reg_c].unsafe_chr)
+        @logger.info(@register[reg_c].unsafe_chr)
       end
     when 11 # Input
       if @input_pos < @input_str.size
@@ -88,16 +96,16 @@ class UniversalMachine
       @dump = @register[reg_c] == 'p'.ord
     when 12 # Load Program
       if @register[reg_b] != 0
-        STDERR.puts("load array:#{@register[reg_b]} finger:#{@register[reg_c]}")
+        @logger.info("load array:#{@register[reg_b]} finger:#{@register[reg_c]}")
         @program = @arrays[@register[reg_b]][0].clone
         @arrays[0_u32] = [@program]
       end
-      STDERR.puts("load program: size:#{@program.size} finger:#{@finger}(#{@finger.to_s(16)})->#{@register[reg_c]}(#{@register[reg_c].to_s(16)})")
+      @logger.info("load program: size:#{@program.size} finger:#{@finger}(#{@finger.to_s(16)})->#{@register[reg_c]}(#{@register[reg_c].to_s(16)})")
       @finger = @register[reg_c]
     when 13 # Orthography
       reg_a = (platter >> 25) & 7
       value = platter & 0x01FF_FFFF
-      STDERR.puts("r[#{reg_a}] <- #{value}")
+      @logger.info("r[#{reg_a}] <- #{value}")
       @register[reg_a] = value
     end
     @finger += 1 unless opcode == 12
