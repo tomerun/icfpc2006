8dea38cf5205849173474e015e03759d3feb168b
tomerun <tomerunmail@gmail.com>
2017-08-17 15:39:30 +0900

    いろいろkaizen
    * ログ文字列を作るところがとても遅かった
    * Plattersは使う必要なかった
    * dumpはコメントアウト

--- um.cr
+++ um.cr
@@ -1,6 +1,7 @@
 require "logger"
 
 SEGMENT_BITS = 10
+DEBUG        = false
 
 class Platters
   @mapping : (Array(Array(UInt32) | Nil) | Nil)
@@ -80,16 +81,16 @@ class UniversalMachine
         (bytes[i * 4 + 3].to_u32 << 0)
     end
     @register = Array(UInt32).new(8, 0_u32)
-    @arrays = Hash(UInt32, (Array(UInt32) | Platters)).new
+    @arrays = Hash(UInt32, Array(UInt32)).new
     @arrays[0_u32] = @program
     @finger = 0_u32
     @rng = Random.new(42)
-    @dump = false
-    @dump_file = File.new("decompressed.um", "w")
-    @input_str = "(\\b.bb)(\\v.vv)06FHPVboundvarHRAk"
+    # @dump = false
+    # @dump_file = File.new("dump.um", "w")
+    @input_str = "guest\n" # "(\\b.bb)(\\v.vv)06FHPVboundvarHRAk"
     @input_pos = 0
     @logger = Logger.new(STDERR)
-    @logger.level = Logger::WARN
+    @logger.level = DEBUG ? Logger::INFO : Logger::WARN
     @logger.formatter = Logger::Formatter.new do |severity, datetime, progname, message, io|
       label = severity.unknown? ? "ANY" : severity.to_s
       io << label[0] << ", " << message
@@ -110,33 +111,33 @@ class UniversalMachine
     opcode = platter >> 28
     case opcode
     when 0 # Conditional Move
-      @logger.info("r[#{reg_a}] <- r[#{reg_b}] if r[#{reg_c}] #{@register}")
+      @logger.info("r[#{reg_a}] <- r[#{reg_b}] if r[#{reg_c}] #{@register}") if DEBUG
       @register[reg_a] = @register[reg_b] if @register[reg_c] != 0
     when 1 # Array Index
       ar = @arrays[@register[reg_b]]
       if ar.size <= @register[reg_c]
         @logger.error("1: #{ar.size}  #{@register[reg_c]} at finger #{@finger}")
       end
-      @logger.info("r[#{reg_a}] = arr[#{@register[reg_b]}][#{@register[reg_c]}] #{@register}")
+      @logger.info("r[#{reg_a}] = arr[#{@register[reg_b]}][#{@register[reg_c]}] #{@register}") if DEBUG
       @register[reg_a] = ar[@register[reg_c]]
     when 2 # Array Amendment
       ar = @arrays[@register[reg_a]]
       if ar.size <= @register[reg_b]
         @logger.error("2: #{ar.size}  #{@register[reg_b]} at finger #{@finger}")
       end
-      @logger.info("arr[#{@register[reg_a]}][#{@register[reg_b]}] = r[#{reg_c}] #{@register}")
+      @logger.info("arr[#{@register[reg_a]}][#{@register[reg_b]}] = r[#{reg_c}] #{@register}") if DEBUG
       ar[@register[reg_b]] = @register[reg_c]
     when 3 # Addition
-      @logger.info("r[#{reg_a}] <- r[#{reg_b}] + r[#{reg_c}] #{@register}")
+      @logger.info("r[#{reg_a}] <- r[#{reg_b}] + r[#{reg_c}] #{@register}") if DEBUG
       @register[reg_a] = @register[reg_b] + @register[reg_c]
     when 4 # Multiplication
-      @logger.info("r[#{reg_a}] <- r[#{reg_b}] * r[#{reg_c}] #{@register}")
+      @logger.info("r[#{reg_a}] <- r[#{reg_b}] * r[#{reg_c}] #{@register}") if DEBUG
       @register[reg_a] = @register[reg_b] * @register[reg_c]
     when 5 # Division
-      @logger.info("r[#{reg_a}] <- r[#{reg_b}] / r[#{reg_c}] #{@register}")
+      @logger.info("r[#{reg_a}] <- r[#{reg_b}] / r[#{reg_c}] #{@register}") if DEBUG
       @register[reg_a] = @register[reg_b] / @register[reg_c]
     when 6 # Not-And
-      @logger.info("r[#{reg_a}] <- ~(r[#{reg_b}] & r[#{reg_c})] #{@register}")
+      @logger.info("r[#{reg_a}] <- ~(r[#{reg_b}] & r[#{reg_c})] #{@register}") if DEBUG
       @register[reg_a] = ~(@register[reg_b] & @register[reg_c])
     when 7 # Halt
       return false
@@ -148,19 +149,19 @@ class UniversalMachine
           break
         end
       end
-      @logger.info("alloc:arr[#{@register[reg_b]}] = new array[#{size}]")
-      @arrays[@register[reg_b]] = Platters.new(size)
+      @logger.info("alloc:arr[#{@register[reg_b]}] = new array[#{size}]") if DEBUG
+      @arrays[@register[reg_b]] = Array.new(size, 0_u32)
     when 9 # Abandonment
-      @logger.info("abandon:#{@register[reg_c]}")
+      @logger.info("abandon:#{@register[reg_c]}") if DEBUG
       raise @register[reg_c].to_s if !@arrays.has_key?(@register[reg_c])
       @arrays.delete(@register[reg_c])
     when 10 # Output
-      if @dump
-        @dump_file.write_byte(@register[reg_c].to_u8)
-      else
-        print @register[reg_c].unsafe_chr
-        @logger.info(@register[reg_c].unsafe_chr)
-      end
+      # if @dump
+      #   @dump_file.write_byte(@register[reg_c].to_u8)
+      # else
+      print @register[reg_c].unsafe_chr
+      @logger.info(@register[reg_c].unsafe_chr) if DEBUG
+      # end
     when 11 # Input
       if @input_pos < @input_str.size
         input = @input_str[@input_pos].ord
@@ -169,20 +170,20 @@ class UniversalMachine
         input = STDIN.read_byte
       end
       @register[reg_c] = input ? input.to_u32 : UInt32::MAX
-      @dump = @register[reg_c] == 'p'.ord
+      # @dump = @register[reg_c] == 'p'.ord
     when 12 # Load Program
       if @register[reg_b] != 0
-        @logger.info("load array:#{@register[reg_b]} finger:#{@register[reg_c]}")
-        @program = @arrays[@register[reg_b]].to_a
+        @logger.info("load array:#{@register[reg_b]} finger:#{@register[reg_c]}") if DEBUG
+        @program = @arrays[@register[reg_b]].clone
         @arrays[0_u32] = @program
       else
-        @logger.info("jump finger:#{@finger}(#{@finger.to_s(16)})->#{@register[reg_c]}(#{@register[reg_c].to_s(16)})")
+        @logger.info("jump finger:#{@finger}(#{@finger.to_s(16)})->#{@register[reg_c]}(#{@register[reg_c].to_s(16)})") if DEBUG
       end
       @finger = @register[reg_c]
     when 13 # Orthography
       reg_a = (platter >> 25) & 7
       value = platter & 0x01FF_FFFF
-      @logger.info("r[#{reg_a}] <- #{value}")
+      @logger.info("r[#{reg_a}] <- #{value}") if DEBUG
       @register[reg_a] = value
     end
     @finger += 1 unless opcode == 12
